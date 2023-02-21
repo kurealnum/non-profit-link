a0c12579daf4e52cdcd45133fe48fde9ae50edd2
kurealnum <cattledoger@gmail.com>
2023-02-21 14:15:37 -0500

    Just some test sql stuff, and basic formatting (i hate sql)

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from forms import search_home_page
 import sqlite3
 
 #connecting to the database
-connection = sqlite3.connect('database.db')
+conn = sqlite3.connect('database.db')
 
 #some basic info to give flask
 app = Flask(__name__, template_folder='templates', static_folder='static')
---
+++ test_sql/schema.sql
@@ -0,0 +1,4 @@
+CREATE TABLE names (
+    first_name TEXT, 
+    last_name TEXT
+);
