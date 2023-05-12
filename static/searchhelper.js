6ddd3e2edaa60603cf3e82e2d78525aaba282dd6
kurealnum <cattledoger@gmail.com>
2023-05-12 11:50:53 -0400

    Some issues with searching. See .js file in static for details.

--- static/searchhelper.js
+++ static/searchhelper.js
@@ -1,7 +1,12 @@
 let input = document.getElementById('search');
     input.addEventListener('input', async function() {
         let zip_code = document.getElementById('search');
-        let search_field = await fetch('/search?request_type=search&zip_code=' + zip_code.value);
+        let search_field = await fetch('/search?zip_code=' + zip_code.value);
         let search_field_text = await search_field.text();
         document.getElementById('search_return').innerHTML = search_field_text;
     });
+
+//figure out why my inputs aren't being picked up
+
+//figure out why search_field_text isn't rendering 
+//on the UL, and its rendering on a seperate page instead
