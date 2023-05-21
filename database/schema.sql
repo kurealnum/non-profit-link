61d19d931f284416070649d475e2af7d8970c30e
kurealnum <cattledoger@gmail.com>
2023-05-21 19:25:09 -0400

    added diagrams (drawio). loc in diagrams folder :)

--- database/schema.sql
+++ database/schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS basic_users (
+CREATE TABLE IF NOT EXISTS users (
     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     username TEXT NOT NULL,
     password_hash TEXT NOT NULL
@@ -9,19 +9,25 @@ CREATE TABLE IF NOT EXISTS searches (
     time_stamp DATETIME
 );
 
+/* org info section */
+
 CREATE TABLE IF NOT EXISTS orgs (
     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     name TEXT NOT NULL, 
-    desc_ TEXT NOT NULL,
-    phone TEXT NOT NULL, /*interpret as an int, and remove the "-" and "()"*/
-    email TEXT NOT NULL,
+    desc_ TEXT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS orgs_loc (
-    id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+    name TEXT PRIMARY KEY AUTOINCREMENT NOT NULL,
     country TEXT NOT NULL,
     region TEXT NOT NULL, /*this just means the state (in the US)*/
     zip INT NOT NULL,
     address TEXT NOT NULL
+);
 
-/* there's a weird CREATE TABLE sqlite_sequence(name,seq); when you create it idk why lol
+CREATE TABLE IF NOT EXISTS orgs_contact (
+    phone TEXT NOT NULL, /*interpret as an int, and remove the "-" and "()"*/
+    email TEXT NOT NULL
+);
+
+/* there's a weird CREATE TABLE sqlite_sequence(name,seq); when you create it idk why lol */
