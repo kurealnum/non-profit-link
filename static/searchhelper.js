ebcc6b40c4fb8f77a8bb20c5bdda7c40116df5f5
kurealnum <cattledoger@gmail.com>
2023-06-07 12:14:45 -0400

    Added a JS function to update the search bar in a different way

--- static/searchhelper.js
+++ static/searchhelper.js
@@ -1,19 +1,28 @@
+//search function that talks to the api and stuff
+async function search() {
+  let zip_code = document.getElementById('search');
+
+  if (zip_code.value != null){
+    // add the users search to a formdata object
+  let search = new FormData()
+  search.append("search", zip_code.value)
+
+  // fetch request + return
+  let search_field = await fetch('/search', {method:'POST', body:search});
+  let search_field_text = await search_field.text();
+  document.getElementById('search_return').innerHTML = search_field_text;
+  }
+}
+
 let input = document.getElementById('search');
-    input.addEventListener('input', async function() {
-        let zip_code = document.getElementById('search');
 
-        // add the users search to a formdata object
-        let search = new FormData()
-        search.append("search", zip_code.value)
+//first event is for the search bar updating, the second is for checking the search bar on page reload
+input.addEventListener('input', search);
+document.addEventListener('DOMContentLoaded', search);
 
-        // fetch request + return
-        let search_field = await fetch('/search', {method:'POST', body:search});
-        let search_field_text = await search_field.text();
-        document.getElementById('search_return').innerHTML = search_field_text;
-    });
-
+//just to prevent the search bar having a panic attack
 document.addEventListener('keydown', function(event) {
   if (event.key === "Enter") {
-    event.preventDefault();
+    event.preventDefault(); 
   }
 });
