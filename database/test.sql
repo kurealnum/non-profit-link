edbbe075c09cc341acb26f26113b3f8f2f459946
kurealnum <cattledoger@gmail.com>
2023-06-14 18:19:42 -0400

    First commit

--- database/test.sql
+++
@@ -1,4 +0,0 @@
-SELECT orgs_loc.org_name, country, region, zip, city, street_address, website 
-FROM orgs_loc 
-    JOIN orgs_info ON orgs_info.org_id = orgs_loc.org_id
-    WHERE SUBSTR(zip,1,1) = '8';
