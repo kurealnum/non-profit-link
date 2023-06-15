e3462966639bfae901b390c018fa49fada8be3cf
kurealnum <cattledoger@gmail.com>
2023-06-14 22:30:39 -0400

    Started working on models, added a basic template

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class basic_account(models.Model):
+    firstname = models.CharField(max_length=255)
+    lastname = models.CharField(max_length=255)
+    
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 def hello_world(request):
-    return HttpResponse("Hello World")
+    template = loader.get_template('register.html')
+    return HttpResponse(template.render())
