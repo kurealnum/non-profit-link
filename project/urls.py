580619173fc011ce06453a79f47daa857c9f1b4d
kurealnum <cattledoger@gmail.com>
2023-08-11 16:07:25 -0400

    Linked 'homepage' to layout.html

--- project/urls.py
+++ project/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('apps.login_register.urls')),
+    path('', include('apps.homepage.urls')),
     path('admin/', admin.site.urls),
 ]
