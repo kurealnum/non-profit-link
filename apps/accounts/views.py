5774947095f9f0cf93a59f20837b4b46f2287d40
kurealnum <cattledoger@gmail.com>
2023-08-14 10:53:06 -0400

    Forgot to add login functionality

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -6,19 +6,29 @@ from .forms import LoginForm
 
 
 def login_user(request):
+    print(request)
 
     if request.method == "POST":
         form = LoginForm(request.POST)
 
         #if the form isn't empty
         if form.is_valid():
+
+            #data
             email = form.cleaned_data["email"]
             password = form.cleaned_data["password"]
             user = authenticate(request, username=email, password=password)
-            print(user)
+            
+            #if authenticate returns a user object, which means that the user is valid
+            if user:
+                login(request, user)
 
     #else is a GET request
     else:
         form = LoginForm()
 
     return render(request, "login.html", {"form": form})
+
+
+def logout_user(request):
+    pass
