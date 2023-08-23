cd1bea93daa63b081cddcc8c1f16d4d97f8b9b13
kurealnum <cattledoger@gmail.com>
2023-08-23 09:52:24 -0400

    Still working on bugs

--- react_frontend/src/App.tsx
+++ react_frontend/src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
@@ -9,10 +8,14 @@ function App() {
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src="frontend_dist/vite.svg" className="logo" alt="Vite logo" />
+          <img src="/frontend_dist/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img
+            src="/frontend_dist/assets/react-35ef61ed.svg"
+            className="logo react"
+            alt="React logo"
+          />
         </a>
       </div>
       <h1>Vite + React</h1>
