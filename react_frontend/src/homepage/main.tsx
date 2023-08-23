70e98359d2e44be66ba67d315151342130b902d7
kurealnum <cattledoger@gmail.com>
2023-08-23 18:21:24 -0400

    More dynamic buttons!

--- react_frontend/src/homepage/main.tsx
+++ react_frontend/src/homepage/main.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import Button from "../components/Button.tsx";
 
 ReactDOM.createRoot(document.getElementById("react-homepage-hero")!).render(
   <React.StrictMode>
@@ -9,11 +10,12 @@ ReactDOM.createRoot(document.getElementById("react-homepage-hero")!).render(
 );
 
 var buttons = document.getElementsByClassName("react-button");
+var buttons_titles = ["Search non-profits", "Search items"];
 
 for (let i = 0; i < buttons.length; i++) {
   ReactDOM.createRoot(buttons[i]!).render(
     <React.StrictMode>
-      <p>Test</p>
+      <Button>{buttons_titles[i]}</Button>
     </React.StrictMode>
   );
 }
