0e59eb2cf60fa62062584902367c2d2fce01cc68
kurealnum <cattledoger@gmail.com>
2023-08-24 16:09:51 -0400

    Added a blob SVG

--- react_frontend/src/homepage/HomepageHero.tsx
+++ react_frontend/src/homepage/HomepageHero.tsx
@@ -5,7 +5,13 @@ import Title from "../components/Title";
 function HomepageHero() {
   return (
     <>
-      <Title highlight_word="non-profits">
+      <Title
+        highlight_word="non-profits"
+        background_img={[
+          "/frontend_dist/assets/title_blob.svg",
+          "Blob image as background for the title",
+        ]}
+      >
         Helping non-profits share their resources since 2023
       </Title>
     </>
