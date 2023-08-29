3f2aa96fde44cc03ad3e0a71f49b80cf041cb205
kurealnum <cattledoger@gmail.com>
2023-08-28 22:19:35 -0400

    it. compiled.

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,14 @@ module.exports = {
         filename: 'index-bundle.js',  // output bundle file name
         path: path.resolve(__dirname, './static'),  // path to our Django static directory
     },
+    module: {
+        rules: [
+          {
+            test: /\.(ts|tsx)$/,
+            exclude: /node_modules/,
+            loader: "babel-loader",
+            options: { presets: ["@babel/preset-env", "@babel/preset-react"] }
+          },
+        ]
+      }
 };
