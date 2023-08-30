a608079845fa00abda584fa2f8cb8cb48097f15b
kurealnum <cattledoger@gmail.com>
2023-08-30 14:22:25 -0400

    All errors fixed, merging

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
 const path = require('path');
 
 module.exports = {
-    entry:'./assets/ts/index.tsx',
+    entry:'./assets/index.tsx',
     output: {
         filename: 'index-bundle.js',  // output bundle file name
         path: path.resolve(__dirname, './static'),  // path to our Django static directory
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx']
     },
     module: {
         rules: [
@@ -12,13 +15,11 @@ module.exports = {
             test: /\.(ts|tsx)$/,
             exclude: /node_modules/,
             loader: "babel-loader",
-            options: { presets: ["@babel/preset-env", "@babel/preset-react"] }
+            options: { presets: ["@babel/preset-env", "@babel/preset-react", "@babel/preset-typescript"] }
           },
           {
             test: /\.(png|jpe?g|gif|svg)$/i,
-            options: {
-              name: '[name].[ext]',
-            },
+       
             use: "file-loader",
           }
         ]
