94d0f884bbe8505ec4543c805f02b7936bee3e7c
kurealnum <cattledoger@gmail.com>
2023-09-01 20:38:15 -0400

    Working on paragraphs

--- assets/components/Title.tsx
+++ assets/components/Title.tsx
@@ -3,15 +3,16 @@ import React from "react";
 interface Props {
   children: string;
   highlight_word: string;
+  id?: string;
   //img src from frontend_dist, and alt
   background_img: string[];
 }
 
-const Title = ({ children, highlight_word, background_img }: Props) => {
+const Title = ({ children, highlight_word, id, background_img }: Props) => {
   const split_children = children.split(highlight_word);
   return (
     <>
-      <h1>
+      <h1 id={id}>
         {split_children[0]}
         <span className="highlight-word">{highlight_word}</span>
         {split_children[1]}
--- assets/pages/index.tsx
+++ assets/pages/index.tsx
@@ -34,11 +34,13 @@ for (let i = 0; i < buttons.length; i++) {
 const paragraphs: paragraphInfo[] = [
   {
     header: "My First Header",
-    paragraph: "My First Paragraph",
+    paragraph:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
   {
     header: "My Second Header",
-    paragraph: "My Second Paragraph",
+    paragraph:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
   },
 ];
 
