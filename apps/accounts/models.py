3dce5bc22430d101612e338911e1a0da8c2552ee
kurealnum <cattledoger@gmail.com>
2023-09-10 21:40:00 -0400

    Another new model, also udpated db schema

--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -3,12 +3,21 @@ from django.contrib.auth.models import AbstractUser
 from .managers import CustomUserManager
 
 
+class orgs_loc(models.Model):
+    country = models.CharField(unique=False, max_length=50)
+    region = models.CharField(unique=False, max_length=50)
+    zip = models.SmallIntegerField(unique=False, null=False)  # can be null
+    city = models.CharField(unique=False, max_length=50)
+    street_address = models.CharField(unique=True, max_length=50)
+
+
 class orgs(AbstractUser):
     objects = CustomUserManager()
 
     non_profit_name = models.CharField(
         unique=False, max_length=100, default="Un-named non-profit :("
     )
+    loc = models.OneToOneField(orgs_loc, on_delete=models.CASCADE, primary_key=True)
     email = models.EmailField(unique=True)
 
     USERNAME_FIELD = "email"
@@ -18,20 +27,7 @@ class orgs(AbstractUser):
         return self.email
 
 
-class orgs_loc(models.Model):
-    country = models.CharField(unique=False, max_length=50)
-    region = models.CharField(unique=False, max_length=50)
-    zip = models.SmallIntegerField(unique=False, null=False)  # can be null
-    city = models.CharField(unique=False, max_length=50)
-    street_address = models.CharField(unique=True, max_length=50)
-
-
-class orgs_items(models.Model):  # junction table for wanted items by orgs
-    org = models.ForeignKey(orgs, on_delete=models.CASCADE)
-    # check drawn schema, not sure how this should work
-
-
 class item(models.Model):  # model for all items
-    org = models.ForeignKey(orgs, on_delete=models.CASCADE)
-    want = models.CharField(unique=False, max_length=100)
-    count = models.SmallIntegerField(unique=False)
+    org = models.ForeignKey(orgs, on_delete=models.CASCADE, primary_key=True)
+    want = models.BooleanField(unique=False, max_length=100)
+    count = models.SmallIntegerField(unique=False, default=1)
