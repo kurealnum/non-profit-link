2f8368cff9ca8bb58c4ca1609654722fa31d95e5
kurealnum <cattledoger@gmail.com>
2023-09-11 17:34:51 -0400

    Changed var names

--- apps/accounts/forms.py
+++ apps/accounts/forms.py
@@ -1,22 +1,24 @@
 from django import forms
 from django.contrib.auth.forms import UserChangeForm, UserCreationForm
-from .models import orgs
+from .models import org
 
 
 class LoginRegisterForm(forms.Form):
     email = forms.EmailField(label="Enter your email", max_length=100)
-    password = forms.CharField(widget=forms.PasswordInput(), label="Enter your password", max_length=100)
+    password = forms.CharField(
+        widget=forms.PasswordInput(), label="Enter your password", max_length=100
+    )
+
 
 class CustomUserCreationForm(UserCreationForm):
-    
     class Meta:
         password2 = None
-        model = orgs
+        model = org
         fields = ("email",)
 
+
 class CustomUserChangeForm(UserChangeForm):
-   
-    class Meta: 
+    class Meta:
         password2 = None
-        model = orgs
+        model = org
         fields = ("email",)
--- apps/accounts/models.py
+++ apps/accounts/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 from .managers import CustomUserManager
 
 
-class orgs_loc(models.Model):
+class org_loc(models.Model):
     country = models.CharField(unique=False, max_length=50)
     region = models.CharField(unique=False, max_length=50)
     zip = models.SmallIntegerField(unique=False, null=False)  # can be null
@@ -11,13 +11,13 @@ class orgs_loc(models.Model):
     street_address = models.CharField(unique=True, max_length=50)
 
 
-class orgs(AbstractUser):
+class org(AbstractUser):
     objects = CustomUserManager()
 
     non_profit_name = models.CharField(
         unique=False, max_length=100, default="Un-named non-profit :("
     )
-    loc = models.OneToOneField(orgs_loc, on_delete=models.CASCADE, primary_key=True)
+    loc = models.OneToOneField(org_loc, on_delete=models.CASCADE, primary_key=True)
     email = models.EmailField(unique=True)
 
     USERNAME_FIELD = "email"
@@ -28,6 +28,6 @@ class orgs(AbstractUser):
 
 
 class item(models.Model):  # model for all items
-    org = models.ForeignKey(orgs, on_delete=models.CASCADE, primary_key=True)
+    org = models.ForeignKey(org, on_delete=models.CASCADE, primary_key=True)
     want = models.BooleanField(unique=False, max_length=100)
     count = models.SmallIntegerField(unique=False, default=1)
