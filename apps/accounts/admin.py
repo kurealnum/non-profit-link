67802ae2bfee35432a05cac8b0cd8f98edc41d2e
kurealnum <cattledoger@gmail.com>
2023-09-18 18:39:17 -0400

    Updated admin panel to contain inlines

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -4,25 +4,32 @@ from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import Org, OrgContactInfo, OrgInfo, OrgLocation, Item
 
 
+# inlines for Org
+class OrgContactInfoInline(admin.TabularInline):
+    model = OrgContactInfo
+
+
+class OrgInfoInline(admin.TabularInline):
+    model = OrgInfo
+
+
+class OrgLocationInline(admin.TabularInline):
+    model = OrgLocation
+
+
+class ItemInline(admin.TabularInline):
+    model = Item
+
+
+# admin registers
 @admin.register(Org)
 class OrgAdmin(admin.ModelAdmin):
-    fieldsets = [
-        (
-            None,
-            {
-                "fields": [
-                    "non_profit_name",
-                ]
-            },
-        )
-    ]
+    inlines = [OrgContactInfoInline, OrgInfoInline, OrgLocationInline, ItemInline]
     username = None
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = Org
-    list_display = [
-        "non_profit_name",
-    ]
+    list_display = ["non_profit_name", "loc"]
     ordering = ("-non_profit_name",)
 
 
