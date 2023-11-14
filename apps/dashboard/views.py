03c8e4859a20a3628aa7dfac8fdc19eeaa85f8b4
kurealnum <cattledoger@gmail.com>
2023-11-14 12:31:04 -0500

    Figuring out how to pass data to JS

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -7,23 +7,26 @@ from apps.accounts.models import Org, OrgContactInfo, OrgInfo, OrgLocation
 @login_required
 def dashboard(request):
     # getting the current user/org info
-    current_user = request.user
-    current_org = Org.objects.get(username=current_user.username)
-    current_org_location = OrgLocation.objects.get(org=current_org)
-    current_org_contact_info = OrgContactInfo.objects.get(org=current_org)
-    current_org_info = OrgInfo.objects.get(org=current_org)
-    current_wanted_org_items = current_org.item_set.filter(want=True)  # type: ignore
-    current_needed_org_items = current_org.item_set.filter(want=False)  # type: ignore not sure why i have to do this, it's a completely valid thingy (whatever you call it)
+    user = request.user
+    org = Org.objects.get(username=user.username)
+    org_location = OrgLocation.objects.get(org=org)
+    org_contact_info = OrgContactInfo.objects.get(org=org)
+    org_info = OrgInfo.objects.get(org=org)
+    wanted_org_items = org.item_set.filter(want=True)  # type: ignore
+    needed_org_items = org.item_set.filter(want=False)  # type: ignore not sure why i have to do this, it's a completely valid thingy (whatever you call it)
 
     return render(
         request,
         "dashboard.html",
         context={
-            "current_org": current_org,
-            "current_org_location": current_org_location,
-            "current_org_contact_info": current_org_contact_info,
-            "current_org_info": current_org_info,
-            "current_wanted_org_items": current_wanted_org_items,
-            "current_needed_org_items": current_needed_org_items,
+            "org": {
+                "org-name": org.username,
+                "org-desc": org_info.desc,
+            },
+            "org_location": org_location,
+            "org_contact_info": org_contact_info,
+            "org_info": org_info,
+            "wanted_org_items": wanted_org_items,
+            "needed_org_items": needed_org_items,
         },
     )
