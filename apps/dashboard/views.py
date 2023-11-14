eba538ff89ecd3e2e34440ef62d6e7d143c86475
kurealnum <cattledoger@gmail.com>
2023-11-13 19:56:24 -0500

    Added type: ignore

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -8,11 +8,11 @@ from apps.accounts.models import Org, OrgContactInfo, OrgInfo, OrgLocation
 def dashboard(request):
     # getting the current user/org info
     current_user = request.user
-    current_org = Org.objects.get(org_name=current_user.org_name)
+    current_org = Org.objects.get(username=current_user.username)
     current_org_location = OrgLocation.objects.get(org=current_org)
     current_org_contact_info = OrgContactInfo.objects.get(org=current_org)
     current_org_info = OrgInfo.objects.get(org=current_org)
-    current_org_items = current_org.item_set.all()
+    current_org_items = current_org.item_set.all()  # type: ignore not sure why i have to do this, it's a completely valid thingy (whatever you call it)
 
     print(current_org_items)
 
