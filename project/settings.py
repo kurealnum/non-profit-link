b73c25f733ff236d1004436aaf124a462ba5919c
kurealnum <cattledoger@gmail.com>
2023-11-14 21:57:58 -0500

    Slowly transfering the homepage back to raw HTML

--- project/settings.py
+++ project/settings.py
@@ -130,7 +130,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "frontend_dist/"
+STATIC_URL = "static/"
 
 STATICFILES_DIRS = [
     BASE_DIR / "react_static",
