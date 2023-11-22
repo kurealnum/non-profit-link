d30c0f3d88d70960c42f5d9807bda5fe507eb99d
kurealnum <cattledoger@gmail.com>
2023-11-22 17:28:37 -0500

    Found a much better way to query. Credit to CodenameTim on the Django discord

--- apps/homepage/views.py
+++ apps/homepage/views.py
@@ -7,22 +7,12 @@ from ..accounts.models import Item, Org
 
 
 def index(request):
-    # getting all items, with
-    top_5_items = Item.objects.values("org", "item_name", "count").order_by("-count")
-
-    # what we'll send in context
-    top_5_items_context = {}
-
-    # getting the top 5 items
-    p = 0
-    while len(top_5_items_context) < 5 and p < len(top_5_items):
-        tmp_item = top_5_items[p]
-        # if item does not exist, set it equal to the current value of that
-        top_5_items_context[tmp_item["item_name"]] = tmp_item[
-            "count"
-        ] + top_5_items_context.get(tmp_item["item_name"], 0)
-
-        p += 1
+    # getting the 5 items with the highest count
+    top_5_items = (
+        Item.objects.values_list("item_name")
+        .annotate(sum=Sum("count"))
+        .order_by("-sum")[:5]
+    )
 
     # getting 5 random models
     total_orgs = Org.objects.count()
@@ -33,5 +23,5 @@ def index(request):
     return render(
         request,
         "home.html",
-        context={"random_5_orgs": random_5_orgs, "top_5_items": top_5_items_context},
+        context={"random_5_orgs": random_5_orgs, "top_5_items": list(top_5_items)},
     )
