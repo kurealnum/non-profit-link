ea2a81ec0b18f8087c5b4766f55d5235d83cc8f7
kurealnum <cattledoger@gmail.com>
2023-11-26 19:39:28 -0500

    Most bugs have been weeded out; making basic homepage now

--- apps/org_pages/urls.py
+++ apps/org_pages/urls.py
@@ -2,4 +2,10 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("dashboard/", views.dashboard, name="dashboard")]
+# NOT COMPLETE!!!
+# homepage needs fancy URL stuff
+
+urlpatterns = [
+    path("dashboard/", views.dashboard, name="dashboard"),
+    path("homepage/", views.homepage, name="homepage"),
+]
