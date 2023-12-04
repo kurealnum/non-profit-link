da6b50a61d733afc2995cf7d9671e8f75c802626
kurealnum <cattledoger@gmail.com>
2023-12-04 16:54:54 -0500

    Blurred background on modal open

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,12 +1,17 @@
 const needsButton = document.getElementById("needs-button")
 const needsModal = document.getElementById("needs-modal")
 const needsCloseButton = document.getElementById("needs-close-button")
+const modalBlur = document.getElementById("needs-modal-blur")
 
 needsButton.onclick = function() {
+    document.documentElement.style.overflow = "hidden"
+    modalBlur.style.display = "block"
     needsModal.style.display = "flex"
 }
 
 needsCloseButton.onclick = function() {
     needsModal.style.display = "none"
+    modalBlur.style.display = "none"
+    document.documentElement.style.overflow = "auto"
 }
 
