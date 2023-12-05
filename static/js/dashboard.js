6e2ef8c51d3e86c919853549511b8d2e557f16b5
kurealnum <cattledoger@gmail.com>
2023-12-04 19:31:27 -0500

    Transition kinda works. Still buggy (recreate by spamming the needsButton

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -5,13 +5,25 @@ const modalBlur = document.getElementById("needs-modal-blur")
 
 needsButton.onclick = function() {
     document.documentElement.style.overflow = "hidden"
-    modalBlur.style.visibility = "visible"
+    modalBlur.style.display = "block"
     needsModal.style.display = "flex"
+
+    // transition in
+    window.setTimeout(function(){
+        modalBlur.style.opacity = 1
+        needsModal.style.opacity = 1
+      },0);
 }
 
 needsCloseButton.onclick = function() {
-    needsModal.style.display = "none"
-    modalBlur.style.visibility = "hidden"
     document.documentElement.style.overflow = "auto"
+    modalBlur.style.opacity = 0
+    needsModal.style.opacity = 0
+
+    // transition out
+    window.setTimeout(() => {
+        modalBlur.style.display = "none"
+        needsModal.style.display = "none"
+    }, 700)
 }
 
