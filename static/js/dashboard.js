6509d343a021691e066ecc50739da351adce270f
kurealnum <cattledoger@gmail.com>
2023-12-04 21:54:47 -0500

    Dialogs are pretty neat!

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -1,29 +1,13 @@
 const needsButton = document.getElementById("needs-button")
-const needsModal = document.getElementById("needs-modal")
+const needsModal = document.querySelector("#needs-modal") //is a dialog element
 const needsCloseButton = document.getElementById("needs-close-button")
 const modalBlur = document.getElementById("needs-modal-blur")
 
 needsButton.onclick = function() {
-    document.documentElement.style.overflow = "hidden"
-    modalBlur.style.display = "block"
-    needsModal.style.display = "flex"
-
-    // transition in
-    window.setTimeout(function(){
-        modalBlur.style.opacity = 1
-        needsModal.style.opacity = 1
-      },0);
+    needsModal.showModal()
 }
 
 needsCloseButton.onclick = function() {
-    document.documentElement.style.overflow = "auto"
-    modalBlur.style.opacity = 0
-    needsModal.style.opacity = 0
-
-    // transition out
-    window.setTimeout(() => {
-        modalBlur.style.display = "none"
-        needsModal.style.display = "none"
-    }, 700)
+    needsModal.close()
 }
 
