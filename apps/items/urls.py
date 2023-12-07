df91fe2395dc3688c17d77c77ea284959663a113
kurealnum <cattledoger@gmail.com>
2023-12-06 20:15:20 -0500

    Revert "Revert "Separated some stuff out to different views""
    This reverts commit d7334984ba21078c491d9716f0c3d6b0838e6cf8.

--- apps/items/urls.py
+++ apps/items/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("search-items/", views.search_items, name="search_items")]
+urlpatterns = [
+    path("search-items/", views.search_items, name="search_items"),
+    path("manage-items/", views.manage_item_formset, name="manage_items"),  # type: ignore
+]
