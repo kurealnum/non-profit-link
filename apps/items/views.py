8f5f3d3b7f16d42e2b5c3ef8ed4f89783064cdcd
kurealnum <cattledoger@gmail.com>
2023-12-07 17:41:40 -0500

    Added some functionality to new views

--- apps/items/views.py
+++ apps/items/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+from .models import Item
+
+API_ERROR_TEMPLATE = "api_error.html"
 
 
 def search_items(request):
     return render(request, "search_items.html")
 
 
+@login_required  # type: ignore
 def add_item(request, item_name):
     # TODO
     if request.method == "POST":
@@ -14,4 +20,7 @@ def add_item(request, item_name):
 def delete_item(request, item_name):
     # TODO
     if request.method == "POST":
-        print("Post")
+        current_user = request.user
+        Item.objects.filter(org=current_user, item_name=item_name).delete()
+    else:
+        return render(request, API_ERROR_TEMPLATE)
