82d81faa709d09012a914313f64e67f2d0c667ce
kurealnum <cattledoger@gmail.com>
2023-12-07 17:01:27 -0500

    Made new views. Might be better to use DRF

--- apps/items/urls.py
+++ apps/items/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("search-items/", views.search_items, name="search_items"),
-    path("manage-items/", views.manage_item_formset, name="manage_items"),  # type: ignore
+    path("add-item/<str:item_name>/", views.add_item, name="add_item"),  # type: ignore
+    path("delete-item/<str:item_name>/", views.delete_item, name="delete_item"),  # type: ignore
 ]
--- apps/items/views.py
+++ apps/items/views.py
@@ -5,13 +5,13 @@ def search_items(request):
     return render(request, "search_items.html")
 
 
-def manage_item_formset(request):
-    print("Managaged")
+def add_item(request, item_name):
     # TODO
-    # check if post or get
-    # if post, then validate forms
     if request.method == "POST":
         print("Post")
-        pass
-        # and do something with the form data to see what's been added, edited, or deleted
-        # send some sort of message that says "saved" (can be thru js or django)
+
+
+def delete_item(request, item_name):
+    # TODO
+    if request.method == "POST":
+        print("Post")
