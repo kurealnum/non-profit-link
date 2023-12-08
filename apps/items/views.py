9d550e1ee768370bbabee733ce63031d154fb633
kurealnum <cattledoger@gmail.com>
2023-12-07 22:29:38 -0500

    Delete should be working. Might be buggy

--- apps/items/views.py
+++ apps/items/views.py
@@ -13,6 +13,7 @@ def search_items(request):
 
 
 class ItemListApiView(APIView):
+    # returns all items
     def get(self, request):
         # getting the current user
         user = request.user
@@ -22,6 +23,7 @@ class ItemListApiView(APIView):
         serializer = ItemSerializer(all_items, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
 
+    # creates a new item
     def post(self, request):
         # getting the current user
         user = request.user
@@ -40,3 +42,17 @@ class ItemListApiView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request):
+        # getting the current user
+        user = request.user
+        org = Org.objects.get(username=user.username)
+        org_id = org.id  # type: ignore
+
+        # getting and deleting the item
+        item_name = request.data.get("item_name")
+        if Item.objects.filter(org=org_id, item_name=item_name).exists():
+            Item.objects.get(org=org_id, item_name=item_name).delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+
+        return Response(status=status.HTTP_400_BAD_REQUEST)
