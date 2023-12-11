092607fb8346089b1e1b8c6c9fc036ac5ff5b580
kurealnum <cattledoger@gmail.com>
2023-12-11 12:51:06 -0500

    POST request functionality on frontend! See TODOs

--- static/js/dashboard.js
+++ static/js/dashboard.js
@@ -51,15 +51,54 @@ surplusCloseButton.onclick = function() {
 // adding a new item to the needs modal (NOT saving it)
 const neededModalItemsList = document.getElementById("needed-items-list")
 const needsNewButton = document.getElementById("needs-new-button")
+let needsPOSTRequest = [];
+
+// html for creating a new item, and rendering a 'completed' new item
+const newItem = `
+<div id='creating-item' class="item">
+    <input id='number-of-units' type='text' value='# of units'>
+    <input id='unit-type' type='text' value='units'> of 
+    <input id='item-name' type='text' value='item'>
+    <button id="create-item">&checkmark;</button>
+</div>
+`
+
+// check for if the user is already inputting a new item
+let isNewItem = false;
 
 needsNewButton.onclick = function() {
-    neededModalItemsList.insertAdjacentHTML('beforeend', `
-    <div class='item'>
-        <input type='text' value='# of units'>
-        <input type='text' value='units'> of 
-        <input type='text' value='item'>
-    </div>`
-    )
+    if (isNewItem) {
+        // tell the user that they can't create multiple unsaved items
+        console.log("You can't do that!")
+    }
+    else {
+        isNewItem = !isNewItem
+        neededModalItemsList.insertAdjacentHTML('beforeend', newItem)
+        const createItem = document.getElementById("create-item")
+
+        // assigning onclick func every time we create a new `newItem` div
+        createItem.onclick = function() {
+            isNewItem = !isNewItem;
+            const numberOfUnits = document.getElementById("number-of-units").value
+            const unitType = document.getElementById("unit-type").value
+            const itemName = document.getElementById("item-name").value
+
+            // TODO adding the data to the post request that we'll make when
+            // the user saves the modal. to do because this isn't the correct format
+            needsPOSTRequest.push(numberOfUnits, unitType, itemName)
+
+            // deleting the old item
+            const oldItem = document.getElementById("creating-item")
+            oldItem.remove()
+
+            // making the new item
+            neededModalItemsList.insertAdjacentHTML('beforeend', `
+                <div class="item"> ${numberOfUnits} ${unitType} of ${itemName} </div>
+            `)
+        }
+    }
 }
+
+
 
 
