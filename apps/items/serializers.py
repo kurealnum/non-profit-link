3995b92db08d362c293e13cb24aac66d2e3a0a34
kurealnum <cattledoger@gmail.com>
2023-12-15 11:31:23 -0500

    Thought I was onto something here. You have to call is_valid() first though

--- apps/items/serializers.py
+++ apps/items/serializers.py
@@ -20,3 +20,13 @@ class ItemSerializer(serializers.ModelSerializer):
 
     # identifying which input to send an error to on the frontend
     # TODO: figure out how to make this work
+    def to_internal_value(self, data):
+        internal_value = super().to_internal_value(data)
+
+        if self.errors:
+            for field, errors in self.errors.items():
+                for error in errors:
+                    error_dict = {"error": error, "input_id": data.get("input_id")}
+                    self.errors[field].append(error_dict)
+
+        return internal_value
