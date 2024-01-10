5fa4790731f37b2cf2bb51463b85ff15e8734d82
kurealnum <cattledoger@gmail.com>
2024-01-10 15:18:10 -0500

    Basic tests created

--- apps/accounts/apps.py
+++ apps/accounts/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class LoginLogoutConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.accounts'
+class AccountsConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "apps.accounts"
