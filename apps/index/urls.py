3fa318c8d5ac1250abb003884359ba5a70c2c3a7
kurealnum <cattledoger@gmail.com>
2024-02-04 20:44:31 -0500

    Added placeholder templates

--- apps/index/urls.py
+++ apps/index/urls.py
@@ -2,4 +2,8 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("", views.index, name="index")]
+urlpatterns = [
+    path("about-us/", views.about_us, name="about_us"),
+    path("faq/", views.faq, name="faq"),
+    path("", views.index, name="index"),
+]
--- apps/index/views.py
+++ apps/index/views.py
@@ -6,3 +6,11 @@ def index(request):
         request,
         "index.html",
     )
+
+
+def about_us(request):
+    return render(request, "about_us.html")
+
+
+def faq(request):
+    return render(request, "faq.html")
